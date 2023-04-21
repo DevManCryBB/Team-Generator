5ab9ea23f8d2e47c84a49947648ed0ba3c1d2b93
DevManCryBB <veronicagriggs3@gmail.com>
2023-04-20 20:27:46 -0700

    updates

---
+++ index.js
@@ -0,0 +1,12 @@
+const { default: inquirer } = require("inquirer");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+// inquirer
+//     .prompt {
+
+// }
+
+const person = new Engineer (2,"boop","v@v.v","string")
+console.log(person)
